34cbbf91f41fef107afa911d39722808b5a104c0
Varsha Ravindra <varsharavindra27@gmail.com>
2018-02-25 19:36:36 +0530

    included user sentence

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask,request
 import threading
 import json
 import requests
+import os.path
+import sys
+import uuid
+
 print("starting server")
 
 app=Flask(__name__)
@@ -11,13 +15,14 @@ token="EAACGHzJZBmDABAJH46EL4XMx90yxhdCXekg0tmvWFVnxWpYEp4nKCZCMvlySgQsCjVfZAZCz
 
 def reply(fb_id,fb_text):
 
+    number,med=main(fb_text)
     data={
     "messaging_type": "RESPONSE",
      "recipient": {
     "id": fb_id
     },
     "message": {
-      "text": fb_text
+      "text": med
     }
     }
     json_data=json.dumps(data)
@@ -50,6 +55,35 @@ def hello_world():
 # #     x=request.get_json()
 # #     print(x)
 # #     return "hello"
+
+try:
+
+    import apiai
+except ImportError:
+    sys.path.append(
+        os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir)
+
+    )
+
+CLIENT_ACCESS_TOKEN = 'ab47593acb7f45c68ca4ffe296db1885'
+
+def main(msg):
+    ai = apiai.ApiAI(CLIENT_ACCESS_TOKEN)
+    request = ai.text_request()
+    request.lang = 'en'  # optional, default value equal 'en'
+    request.session_id = str(uuid.uuid1())
+    request.query = msg
+    response = request.getresponse()
+    data = response.read()
+
+    data1 = json.loads(data)
+    data2=data1["result"]["parameters"]["number"]
+    data3=data1["result"]["parameters"]["medicines"]
+    return data2,data3
+# print(data['res'])
+#json.loads(var1)
+
+
 
 
 if __name__ == '__main__':
